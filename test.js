8f1fa03b664e459d90bc32f2ce7c17f7f3bfb54b
RoSosa <113529620+RoSosa@users.noreply.github.com>
2023-11-14 23:29:47 -0300

    Add files via upload

--- test.js
+++ test.js
@@ -10,22 +10,22 @@ async function testProductManager() {
   console.log({ products: await productManager.getProducts() });
 
 
-  await productManager.addProduct("Black Shirt", "A classic black shirt is a wardrobe staple, and this one is no exception. Featuring a flattering fit and a timeless design, this shirt is perfect for any occasion.r", 3000, "https://firebasestorage.googleapis.com/v0/b/estonoesmoda-34be0.appspot.com/o/products%2Fblack-shirt.jpg?alt=media&token=d9694583-cdad-4e01-a6a9-cedad9127ae4&_gl=1*l4f6qh*_ga*NTYwODIwNDQyLjE2OTY4OTkwODY.*_ga_CW55HF8NVT*MTY5Njk4NTUwNy41LjEuMTY5Njk5MTkwMS42MC4wLjA.", "BS", 8);
-  await productManager.addProduct("Square Dress", "Step into timeless elegance with this vintage-inspired grey dress, perfect for special occasions and garden parties.", 5000, "https://firebasestorage.googleapis.com/v0/b/estonoesmoda-34be0.appspot.com/o/products%2Fdressgreysquare.webp?alt=media&token=e157a218-6e9f-4ac6-ab3a-10c555393cbe&_gl=1*e8tva*_ga*NTYwODIwNDQyLjE2OTY4OTkwODY.*_ga_CW55HF8NVT*MTY5Njk4NTUwNy41LjEuMTY5Njk5MjA1MS40NC4wLjA.", "SD", 2);
+  await productManager.addProduct("Black Shirt", "A classic black shirt is a wardrobe staple, and this one is no exception. Featuring a flattering fit and a timeless design, this shirt is perfect for any occasion.r", 3000, "https://im.uniqlo.com/global-cms/spa/res9f736c9e8b74b06eeed863832f7d92cefr.jpg", "BS", 8);
+  await productManager.addProduct("Square Dress", "Step into timeless elegance with this vintage-inspired grey dress, perfect for special occasions and garden parties.", 5000, "https://im.uniqlo.com/global-cms/spa/res9f736c9e8b74b06eeed863832f7d92cefr.jpg", "SD", 2);
 
 
   console.log({ productsAfterInsert: await productManager.getProducts() });
 
 
   try {
-    await productManager.addProduct("", "Your new go-to pair of premium denim jeans, offering a comfortable and flattering fit for versatile, stylish looks.", 5000, "https://firebasestorage.googleapis.com/v0/b/estonoesmoda-34be0.appspot.com/o/products%2Fbluejeans.jpg?alt=media&token=2de6980f-9dd1-454b-9d4d-cd099e31df8b&_gl=1*nzokzp*_ga*NTYwODIwNDQyLjE2OTY4OTkwODY.*_ga_CW55HF8NVT*MTY5Njk4NTUwNy41LjEuMTY5Njk5MTk4MC41NS4wLjA.", "BJ", 3);
+    await productManager.addProduct("", "Your new go-to pair of premium denim jeans, offering a comfortable and flattering fit for versatile, stylish looks.", 5000, "https://hmuruguay.vtexassets.com/arquivos/ids/1353785-483-725/Skinny-High-Jeans---Dark-denim-blue---H-M-UY.jpg?v=638065865013800000", "BJ", 3);
   } catch (error) {
     console.log({ error: error.message });
   }
 
 
   try {
-    await productManager.addProduct("Blue Jeans", "Your new go-to pair of premium denim jeans, offering a comfortable and flattering fit for versatile, stylish looks.", 5000, "https://firebasestorage.googleapis.com/v0/b/estonoesmoda-34be0.appspot.com/o/products%2Fbluejeans.jpg?alt=media&token=2de6980f-9dd1-454b-9d4d-cd099e31df8b&_gl=1*nzokzp*_ga*NTYwODIwNDQyLjE2OTY4OTkwODY.*_ga_CW55HF8NVT*MTY5Njk4NTUwNy41LjEuMTY5Njk5MTk4MC41NS4wLjA.", "SD", 3);
+    await productManager.addProduct("Blue Jeans", "Your new go-to pair of premium denim jeans, offering a comfortable and flattering fit for versatile, stylish looks.", 5000, "https://hmuruguay.vtexassets.com/arquivos/ids/1353785-483-725/Skinny-High-Jeans---Dark-denim-blue---H-M-UY.jpg?v=638065865013800000", "SD", 3);
   } catch (error) {
     console.log({ error: error.message });
   }
@@ -44,11 +44,10 @@ async function testProductManager() {
   try {
     const updatedProduct = await productManager.updateProduct(
       1,
-      "Black Shirt Modify",
+      "black t shirt",
       "A classic black Modify shirt is a wardrobe staple, and this one is no exception. Featuring a flattering fit and a timeless design, this shirt is perfect for any occasion.r",
       6000,
-      "https://firebasestorage.googleapis.com/v0/b/estonoesmoda-34be0.appspot.com/o/products%2Fblack-shirt.jpg?alt=media&token=d9694583-cdad-4e01-a6a9-cedad9127ae4&_gl=1*l4f6qh*_ga*NTYwODIwNDQyLjE2OTY4OTkwODY.*_ga_CW55HF8NVT*MTY5Njk4NTUwNy41LjEuMTY5Njk5MTkwMS42MC4wLjA.",
-      "BSM",
+      "https://im.uniqlo.com/global-cms/spa/res9f736c9e8b74b06eeed863832f7d92cefr.jpg",
       7
     );
     console.log({ updatedProduct });
